5e5076d31b07903af15644b3f78bf5c7a3fcf557
Naveen <naveen@bosswebtech.com>
2014-04-18 19:47:59 +0530

    rt
    tryhyr

---
+++ test/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  test
+//
+//  Created by BOSS Webtech on 18/04/14.
+//  Copyright (c) 2014 BOSS Webtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
---
+++ test/AViewController.m
@@ -0,0 +1,41 @@
+//
+//  AViewController.m
+//  test
+//
+//  Created by BOSS Webtech on 18/04/14.
+//  Copyright (c) 2014 BOSS Webtech. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    NSLog(@"A : test 4");
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
